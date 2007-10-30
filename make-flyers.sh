7d57cbb254354398bbfdae11a7afc250f7ac0cb5
Damien Cassou <damien.cassou@gmail.com>
2007-10-30 10:54:00 +0000

    - flyer.tex is compilable again
    git-svn-id: https://svn.squeak.org/Advertisement/flyers/smalltalk@54 c37776b8-6735-0410-831e-98cbf79b1075

--- make-flyers.sh
+++ make-flyers.sh
@@ -1,9 +1,10 @@
 #! /bin/bash
 
 for language in english french german; do
-	#sed -e 's/^\\input{.*} %%$/\\input{'$language'}/' flyer.tex \
-	sed -e 's/@@LANG@@/'$language'/g' flyer.tex \
-		> smalltalk-flyer-$language.tex
+	sed \
+	    -e 's/^\\input{.*} %%@@LANG@@$/\\input{'$language'}/' \
+	    -e 's/^\\usepackage\[.*\]{babel} %%@@LANG@@$/\\usepackage[english,'$language']{babel}/' \
+	    flyer.tex > smalltalk-flyer-$language.tex
 	pdflatex smalltalk-flyer-$language.tex
 	pdflatex smalltalk-flyer-$language.tex
 	rm -f smalltalk-flyer-$language.{tex,aux,log}
