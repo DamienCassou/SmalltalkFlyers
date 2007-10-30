9d576d98d50403a26c6a666588fc9df816927538
Damien Cassou <damien.cassou@gmail.com>
2007-10-30 10:28:39 +0000

    - Thanks to Damien Pollet, spaces before $: should be handled properly
    git-svn-id: https://svn.squeak.org/Advertisement/flyers/smalltalk@53 c37776b8-6735-0410-831e-98cbf79b1075

--- make-flyers.sh
+++ make-flyers.sh
@@ -1,9 +1,10 @@
 #! /bin/bash
 
 for language in english french german; do
-    sed -e 's/^\\input{.*} %%$/\\input{'$language'}/' flyer.tex \
-	> smalltalk-flyer-$language.tex
-    pdflatex smalltalk-flyer-$language.tex
-    pdflatex smalltalk-flyer-$language.tex
-    rm -f smalltalk-flyer-$language.{tex,aux,log}
+	#sed -e 's/^\\input{.*} %%$/\\input{'$language'}/' flyer.tex \
+	sed -e 's/@@LANG@@/'$language'/g' flyer.tex \
+		> smalltalk-flyer-$language.tex
+	pdflatex smalltalk-flyer-$language.tex
+	pdflatex smalltalk-flyer-$language.tex
+	rm -f smalltalk-flyer-$language.{tex,aux,log}
 done
