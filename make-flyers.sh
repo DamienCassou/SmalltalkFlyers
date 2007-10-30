24f222d47afb8c4ac9498dbd7fdee14539e4e4aa
Damien Cassou <damien.cassou@gmail.com>
2007-10-30 13:09:21 +0000

    - Damien Pollet uses tables instead of description
    git-svn-id: https://svn.squeak.org/Advertisement/flyers/smalltalk@55 c37776b8-6735-0410-831e-98cbf79b1075

--- make-flyers.sh
+++ make-flyers.sh
@@ -5,7 +5,7 @@ for language in english french german; do
 	    -e 's/^\\input{.*} %%@@LANG@@$/\\input{'$language'}/' \
 	    -e 's/^\\usepackage\[.*\]{babel} %%@@LANG@@$/\\usepackage[english,'$language']{babel}/' \
 	    flyer.tex > smalltalk-flyer-$language.tex
-	pdflatex smalltalk-flyer-$language.tex
-	pdflatex smalltalk-flyer-$language.tex
+	pdflatex smalltalk-flyer-$language.tex \
+		&& pdflatex smalltalk-flyer-$language.tex
 	rm -f smalltalk-flyer-$language.{tex,aux,log}
 done
