f63d6948877df5899256b2b5fc1dccba21cfd728
Damien Cassou <damien.cassou@gmail.com>
2007-11-24 18:51:37 +0000

    - Should compile now
    git-svn-id: https://svn.squeak.org/Advertisement/flyers/smalltalk@62 c37776b8-6735-0410-831e-98cbf79b1075

--- make-flyers.sh
+++ make-flyers.sh
@@ -2,7 +2,7 @@
 
 for language in english french german spanish; do
 	sed \
-	    -e 's/^\\input{.*} %%@@LANG@@$/\\input{'$language'}/' \
+	    -e 's/^\\input{.*} %%@@LANG@@$/\\input{languages\/'$language'}/' \
 	    -e 's/^\\usepackage\[.*\]{babel} %%@@LANG@@$/\\usepackage[english,'$language']{babel}/' \
 	    flyer.tex > smalltalk-flyer-$language.tex
 	pdflatex smalltalk-flyer-$language.tex \
